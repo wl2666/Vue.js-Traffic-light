75c7c267a1cfbffcb71bcea885e28ca9de89e097
Vicky <wl2666@tc.columbia.edu>
2019-12-27 15:38:53 -0500

    update vue.js

--- app.js
+++ app.js
@@ -1,24 +1,29 @@
-document.getElementById('stopButton').onclick = illuminateRed;
-document.getElementById('slowButton').onclick = illuminateYellow;
-document.getElementById('goButton').onclick = illuminateGreen;
-
-function illuminateRed() {
-  clearLights();
-  document.getElementById('stopLight').style.backgroundColor = "red";
-}
-
-function illuminateYellow() {
-  clearLights();
-  document.getElementById('slowLight').style.backgroundColor = "yellow";
-}
-
-function illuminateGreen() {
-  clearLights();
-  document.getElementById('goLight').style.backgroundColor = "green";
-}
-
-function clearLights() {
-  document.getElementById('stopLight').style.backgroundColor = "black";
-  document.getElementById('slowLight').style.backgroundColor = "black";
-  document.getElementById('goLight').style.backgroundColor = "black";
-}
+var app = new Vue({
+  el: "#app",
+  data: {
+    title: "Traffic light demo",
+    isClicked: false,
+    turnYellow: false,
+    redLight: {
+      activeColor: "red",
+      inactiveColor: "black"
+    },
+    yellowLight: {
+      activeColor: "yellow",
+      inactiveColor: "black"
+    },
+    greenLight: {
+      activeColor: "green",
+      inactiveColor: "black"
+    }
+  },
+  methods: {
+    changeLightColor() {
+      var v = this;
+      v.isClicked = true;
+      setTimeout(function () {
+        v.turnYellow = true;
+      }, 2000);
+    }
+  }
+});
